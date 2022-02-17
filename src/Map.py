fe273df4ec1c9d278057775d99b1946cbe80ee2e
Tytynguyen <tytynguyens2000@yahoo.com>
2022-02-16 23:05:10 -0800

    Map framework

---
+++ src/Map.py
@@ -0,0 +1,27 @@
+#   Map.py
+#
+#   Stores data structures for Map and Nodes
+
+import numpy as np
+
+class Node:
+    def __init__(self, x, y, neighbors):
+        # Save the state matching this node.
+        self.x = x
+        self.y = y
+
+        # TODO: Figure out what to store here
+        self.neighbors = neighbors  # {(Node,Probability of Wall),(Node,Probability of Wall),...}
+
+    # Report distance to another node (Euclidean)
+    def Dist(self, other):
+        return np.sqrt((other.x - self.x)**2 + (other.y - self.y)**2)
+
+    # TODO: Report probability between 0-1 of a wall existing between two nodes
+    def ProbWall(self, other):
+        # Perhaps use Dist() as a metric? Low distance between two nodes -> high probability of wall
+        # TODO: Problem if distance is less than 1! Fix
+        return 1/self.Dist(other)
+
+
+# TODO: class Map
