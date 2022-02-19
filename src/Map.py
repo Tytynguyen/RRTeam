aed7a92a75b59528f2e1d05f1e4646e1f1783d81
Chase <chasebh3666@gmail.com>
2022-02-19 14:33:36 -0800

    Updated Map to use Segment class and added visualization test method

--- src/Map.py
+++ src/Map.py
@@ -3,6 +3,7 @@
 #   Stores data structures for Map and Nodes
 
 import numpy as np
+from planarutils import *
 
 class Node:
     def __init__(self, x, y, neighbors):
@@ -11,7 +12,7 @@ class Node:
         self.y = y
 
         # TODO: Figure out what to store here
-        self.neighbors = neighbors  # {(Node,Probability of Wall),(Node,Probability of Wall),...}
+        self.neighbors = neighbors  # {Segment, Segment, Segment...}
 
     # Report distance to another node (Euclidean)
     def Dist(self, other):
@@ -23,5 +24,24 @@ class Node:
         # TODO: Problem if distance is less than 1! Fix
         return 1/self.Dist(other)
 
+class Segment:
+    def __init__(self, Node1, Node2):
+        # TODO add option to use a provided probability to initialize instead of calculating probability between the nodes
+        # Save the state matching this segment between nodes
+        self.seg = ((Node1.x, Node1.y), (Node2.x, Node2.y))
+        self.prob  = Node1.ProbWall(Node2)
+
+    def SetProb(self, prob):
+        self.prob = prob
 
 # TODO: class Map
+
+def TestVisualization():
+    # Gimme some test segments to visualize.
+    segments = () # TODO fill these out and decide how big space will be
+
+def main():
+    TestVisualization()
+
+if __name__== "__main__":
+    main()
