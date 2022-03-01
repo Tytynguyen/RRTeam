d796854182db7554907a13e15801b4d5df619be9
Tytynguyen <tytynguyens2000@yahoo.com>
2022-03-01 13:15:27 -0800

    Create RRTStar.py

---
+++ src/RRTStar.py
@@ -0,0 +1,164 @@
+#
+#   RRTStar.py
+#
+#   Search algorithm based upon D* utilizing RRT sampling, developed for dynamic map generation
+#
+#   Procedure:
+#   Given the location of the robot and the goal,
+#   and given a map of the current know world barrier of nodes and probability of a wall existing between a node:
+#
+#   Start all EDGES to ONDECK
+#
+#   Loop:
+#       if ONDECK = empty -> Failure
+#       Pick lowest cost on deck
+#           Cost = distance to goal and wall probability metric
+#
+#           Goto(ONDECK.xcenter,ONDECK.ycener)
+#
+#           If can't go to center of edge
+#               Update edge probability
+#
+
+import random
+
+import numpy as np
+from src.planarutils import *
+from src.utilities import *
+
+dstep = 5
+deadreckoning = 0.05
+Nmax = 1000
+
+
+class Node:
+    """
+    Tree node class that stores parent information and the point vector
+    """
+    def __init__(self, point, parentnode, childrennodes):
+        self.point = point
+        self.parent = parentnode
+        self.children = childrennodes
+
+
+def RRT(tree, goalpoint, Nmax, xmin, xmax, ymin, ymax):
+    """
+    Generate a RRT for use in planning.
+
+    @param tree List of all tree nodes
+    @param startpoint Where tree should start growing from (The end goal)
+    @param goalpoint Where tree should attempt to grow to (The robot)
+    @param Nmax Maximum number of nodes in tree allowed
+    @param xmin
+    @param xmax
+    @param ymin
+    @param ymax
+
+    @return tree list of nodes
+    """
+    #TODO: Check if goal connects to start
+
+    # Loop.
+    while True:
+        # Determine the target point.
+        # Try directly towards the goal by deadreckoning some times, but otherwise
+        # Choose a uniformly random target
+        if random.uniform(0.0, 1.0) <= deadreckoning:
+            # Pick goal as target
+            targetpoint = goalpoint
+        else:
+            # Uniformly pick target
+            x = random.uniform(xmin, xmax)
+            y = random.uniform(ymin, ymax)
+            targetpoint = Point(x, y)
+
+        # Find the nearest node TODO: Make this more efficient...
+        list = [(node.point.dist(targetpoint), node) for node in tree]
+        (d2, nearnode)  = min(list)
+        nearpoint = nearnode.point
+
+        # Determine the next point, a step size (dstep) away.
+        t = np.arctan2((targetpoint.y - nearpoint.y), (targetpoint.x - nearpoint.x))
+        nx = dstep*np.cos(t) + nearpoint.x
+        ny = dstep*np.sin(t) + nearpoint.y
+        nextpoint = Point(nx, ny)
+        nextnode = Node(nextpoint, nearnode, [])
+        tree.append(nextnode)
+
+            # Check whether to attach (creating a new node).
+        if map.localPlanner(nearpoint, nextpoint):
+            nextnode = Node(nextpoint, nearnode, [])
+            tree.append(nextnode)
+
+            nearnode.children.append(nextnode)
+
+            # Also try to connect the goal.
+            if map.localPlanner(nextpoint, goalpoint):
+                goalnode = Node(goalpoint, nextnode)
+                tree.append(goalnode)
+                nextnode.children.append(goalnode)
+
+                return tree
+
+        # Check whether we should abort (tree has gotten too large).
+        if (len(tree) >= Nmax):
+            return None
+
+
+def getPathSegments(node):
+    """
+    Get the segments from the given node to the base of the tree
+    """
+    segments = []
+
+    while node.parent is not None:
+        segments.append(Segment(node,node.parent))
+        node = node.parent
+
+    return segments
+
+def getPathNodes(node):
+    nodes = []
+
+    while node.parent is not None:
+        nodes.append(node)
+        node = node.parent
+
+    return nodes
+
+
+def TStar(startnode, goalnode, map, robot):
+    """
+    Run TStar given a start node and end node, using RRT to generate a tree.
+    """
+    # Build initial tree
+
+    Nmax = 1000
+    xmin = map.xlim[0]
+    xmax = map.xlim[1]
+    ymin = map.ylim[0]
+    ymax = map.ylim[1]
+    tree = RRT([startnode], goalnode, Nmax, xmin, xmax, ymin, ymax)
+
+
+    for curnodei in range(len(movenodes)-1,0):
+        curnode = tree[curnodei]
+
+        # Fails to make it
+        if not robot.goto(curnode.point):
+            p = robot.pos
+            # kill all children
+            curnode.children = []
+        else:
+            return tmovenodes
+
+
+
+
+
+
+
+
+
+
+
